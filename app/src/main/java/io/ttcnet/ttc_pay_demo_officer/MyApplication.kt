b5ceb942b500837bd42bc1e428ce704a5eb16e30
wuqiong <wuqiong@tataufo.com>
2019-02-19 17:24:18 +0800

    release v0.0.19. remove unused resources.

--- app/src/main/java/io/ttcnet/ttc_pay_demo_officer/MyApplication.kt
+++ app/src/main/java/io/ttcnet/ttc_pay_demo_officer/MyApplication.kt
@@ -7,10 +7,11 @@ import io.ttcnet.ttc_pay_demo_officer.util.Utils
 /**
  * Created by lwq on 2018/12/18.
  */
-class Application : Application() {
+class MyApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
         TTCPay.init(applicationContext, "", Utils.getAppId(), Utils.getTTCPublicKey(this), Utils.getSymmetricKey(this))
+        TTCPay.setEnvProd(this, BuildConfig.BUILD_TYPE.equals("release"))
     }
 }
